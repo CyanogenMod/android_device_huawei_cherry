redmi2/android_device_hm2014811_32

mixer_path: Update the mixer file for the tomtom based targets
Update the mixer file for the tomtom based targets.

Change-Id: I3949945cd3e26dd485f8bd53bb65d0bcca6fe809

msm8916_32.mk
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8916_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
     device/qcom/msm8916_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
     device/qcom/msm8916_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
+    device/qcom/msm8916_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml \
     device/qcom/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # NFC packages

CyanogenMod/android_device_huawei_cherry

msm8916_32.mk
@@ -37,7 +37,8 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/qcom/msm8916_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     device/qcom/msm8916_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-    device/qcom/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+    device/qcom/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    device/qcom/msm8916_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml
 
 # ANT+ stack
 PRODUCT_PACKAGES += \