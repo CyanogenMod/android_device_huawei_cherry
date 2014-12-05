f29b59722c01903debd11403badd03122503228b
Meng Wang <mwang@codeaurora.org>
2014-12-05 14:55:06 +0800

    msm8939: audio: add mixer control for MSM8939 SKUL
    Add mixer control for MSM8939 SKUL
    
    Change-Id: I392bdb5610324ec36b68d8c9ae6de8a624cf4e12
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- msm8916_32.mk
+++ msm8916_32.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8916_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
     device/qcom/msm8916_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
     device/qcom/msm8916_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
+    device/qcom/msm8916_32/mixer_paths_skul.xml:system/etc/mixer_paths_skul.xml \
     device/qcom/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/qcom/msm8916_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     device/qcom/msm8916_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
