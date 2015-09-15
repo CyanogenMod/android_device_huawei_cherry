1f8af95ee19171bbc6a3e53dacaff391acddf40e
dianlujitao <dianlujitao@gmail.com>
2015-09-15 20:11:59 +0800

    cherry: Rename defconfig
    Change-Id: I2327c13f5e519c4116d1ebdacbf0acc242cc2a52

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/huawei/msm8916
-TARGET_KERNEL_CONFIG := hw8916_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_cherry_defconfig
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 
 # Lights
