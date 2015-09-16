870ce6236a2d77dbaa6e45b7f19c34953ba74dec
dianlujitao <dianlujitao@gmail.com>
2015-09-16 09:52:28 +0800

    [WIP]cherry: More house cleaning
    Change-Id: Ia179890c2ccf46a311d29e41578197c49609e0ea

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -34,14 +34,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.carrier.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
