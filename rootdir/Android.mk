ba4a30969c655be7656cbe326c8f56682824b8f7
dianlujitao <dianlujitao@gmail.com>
2016-01-06 20:11:34 +0800

    [WIP]cherry: Initial M bring up
     * Mostly updated from LA.BR.1.2.7-00110-8x16.0
    
    Change-Id: Ibc6fc29d1e00d9b494953afd7ebc6db7f3010b6f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,18 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -16,6 +31,13 @@ LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qti.ims.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qti.ims.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
