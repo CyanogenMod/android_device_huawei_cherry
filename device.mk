b72c3d25fc9fa8ebc78844ca780b454540afbfc7
dianlujitao <dianlujitao@gmail.com>
2016-02-04 11:13:22 +0800

    cherry: Import keyhandler from nx510j
     * I don't want to modify kernel to return KEY_POWER for double tap, or we
       won't be able to use proximity check on wake for dt2w.
     * Currently support dt2w only, may add more gestures in the future.
    
    Change-Id: Ic920dc42c79a99da0d83104ad4519a8a321b1097

--- device.mk
+++ device.mk
@@ -150,6 +150,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keyhandler/Gestures
+PRODUCT_PACKAGES += \
+    com.cyanogenmod.keyhandler
+
+PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+
+# never dexopt the keyhandler
+$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
