261cd7fd8862a06b6a709b1c5fe3df5b63a539b8
dianlujitao <dianlujitao@gmail.com>
2016-02-04 13:52:54 +0800

    cherry: keylayout:  Add qpnp_pon.kl
    Change-Id: I8902704f0083e7a851ab3615e1b2223cb66b0b4d

--- device.mk
+++ device.mk
@@ -162,6 +162,7 @@ $(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # Keystore
