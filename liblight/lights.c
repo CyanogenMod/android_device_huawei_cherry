f8b17631fba8953ad57e3893deb3d7f627c7d3d3
dianlujitao <dianlujitao@gmail.com>
2016-02-10 19:11:52 +0800

    cherry: Update lights hal for kernel change
     * Also add permissions to led sysfs node
    
    Change-Id: I9489949250823545128e5175815b387474051b88

--- liblight/lights.c
+++ liblight/lights.c
@@ -50,14 +50,8 @@ char const*const GREEN_LED_FILE
 char const*const BLUE_LED_FILE
         = "/sys/class/leds/blue/brightness";
 
-char const*const BLUETOOTH_LED_FILE
-        = "/sys/class/leds/bt/brightness";
-
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
-
-char const*const BUTTON_FILE
-        = "/sys/class/leds/button-backlight/brightness";
 
 char const*const RED_BLINK_FILE
         = "/sys/class/leds/red/blink";
@@ -67,6 +61,24 @@ char const*const GREEN_BLINK_FILE
 
 char const*const BLUE_BLINK_FILE
         = "/sys/class/leds/blue/blink";
+
+char const*const RED_BLINK_ONMS_FILE
+        = "/sys/class/leds/red/onMS";
+
+char const*const GREEN_BLINK_ONMS_FILE
+        = "/sys/class/leds/green/onMS";
+
+char const*const BLUE_BLINK_ONMS_FILE
+        = "/sys/class/leds/blue/onMS";
+
+char const*const RED_BLINK_OFFMS_FILE
+        = "/sys/class/leds/red/offMS";
+
+char const*const GREEN_BLINK_OFFMS_FILE
+        = "/sys/class/leds/green/offMS";
+
+char const*const BLUE_BLINK_OFFMS_FILE
+        = "/sys/class/leds/blue/offMS";
 
 /**
  * device methods
@@ -166,21 +178,18 @@ set_speaker_light_locked(struct light_device_t* dev,
     blue = colorRGB & 0xFF;
 
     if (onMS > 0 && offMS > 0) {
-        /*
-         * if ON time == OFF time
-         *   use blink mode 2
-         * else
-         *   use blink mode 1
-         */
-        if (onMS == offMS)
-            blink = 2;
-        else
-            blink = 1;
+        blink = 1;
     } else {
         blink = 0;
     }
 
     if (blink) {
+        write_int(RED_BLINK_ONMS_FILE, onMS);
+        write_int(GREEN_BLINK_ONMS_FILE, onMS);
+        write_int(BLUE_BLINK_ONMS_FILE, onMS);
+        write_int(RED_BLINK_OFFMS_FILE, offMS);
+        write_int(GREEN_BLINK_OFFMS_FILE, offMS);
+        write_int(BLUE_BLINK_OFFMS_FILE, offMS);
         if (red) {
             if (write_int(RED_BLINK_FILE, blink))
                 write_int(RED_LED_FILE, 0);
@@ -194,6 +203,9 @@ set_speaker_light_locked(struct light_device_t* dev,
                 write_int(BLUE_LED_FILE, 0);
 	}
     } else {
+        write_int(RED_BLINK_FILE, 0);
+        write_int(GREEN_BLINK_FILE, 0);
+        write_int(BLUE_BLINK_FILE, 0);
         write_int(RED_LED_FILE, red);
         write_int(GREEN_LED_FILE, green);
         write_int(BLUE_LED_FILE, blue);
@@ -249,34 +261,6 @@ set_light_attention(struct light_device_t* dev,
     return 0;
 }
 
-static int
-set_light_buttons(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    int err = 0;
-    if(!dev) {
-        return -1;
-    }
-    pthread_mutex_lock(&g_lock);
-    err = write_int(BUTTON_FILE, state->color & 0xFF);
-    pthread_mutex_unlock(&g_lock);
-    return err;
-}
-
-static int
-set_light_bluetooth(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    int err = 0;
-    if(!dev) {
-        return -1;
-    }
-    pthread_mutex_lock(&g_lock);
-    err = write_int(BLUETOOTH_LED_FILE, state->color & 0xFF);
-    pthread_mutex_unlock(&g_lock);
-    return err;
-}
-
 /** Close the lights device */
 static int
 close_lights(struct light_device_t *dev)
@@ -307,12 +291,8 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
-    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
-        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
-    else if (0 == strcmp(LIGHT_ID_BLUETOOTH, name))
-        set_light = set_light_bluetooth;
     else
         return -EINVAL;
 
