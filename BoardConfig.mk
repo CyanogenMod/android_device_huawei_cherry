31737f19e46da88b1e0371b979c4094d777cbde6
dianlujitao <dianlujitao@gmail.com>
2016-02-21 04:59:27 -0800

    cherry: Disable dexpreopt for now
     * Some users complain to me that sometimes they get stuck in
       the boot animation after installing xposed, disable this to
       make them happy.
    
    Change-Id: I5af0c899ae8913b210919d34de108ccfd97c1836

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,18 +78,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touch_screen/easy_wakeup_gesture"
 #Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-    WITH_DEXPREOPT_PIC := true
-    ifneq ($(TARGET_BUILD_VARIANT),user)
-      # Retain classes.dex in APK's for non-user builds
-      DEX_PREOPT_DEFAULT := nostripping
-    endif
-  endif
-endif
-
 # Flags
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 COMMON_GLOBAL_CPPFLAGS += -DNO_SECURE_DISCARD
