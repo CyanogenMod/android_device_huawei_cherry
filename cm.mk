9fe2b6cdc942eeae581b98a356f5de7531d55759
paranoia gu <gu.yanfeng@gmail.com>
2016-03-15 23:00:01 +0800

    cherry: Add honor 4x support
    Change-Id: Iab7aa389bb0aea6f265189b015421135561a9d3c

--- cm.mk
+++ cm.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/huawei/cherry/device.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
