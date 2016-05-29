414f6babff366557638c01c2b3a4219b968e0a20
dianlujitao <dianlujitao@gmail.com>
2016-05-29 12:33:02 +0800

    cherry: Refactor init extension in C++ way
     * Also add G620S_L02.
    
    Change-Id: Ib9a9e2bd28d0d4f69c4354d294a7a0444df70dc1

--- init/init_cherry.cpp
+++ init/init_cherry.cpp
@@ -27,9 +27,9 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <fstream>
+#include <string>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -41,8 +41,8 @@
 void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
 {
     char platform[PROP_VALUE_MAX];
-    char model[110];
-    FILE* fp;
+    std::ifstream fin;
+    std::string buf;
     int rc;
 
     UNUSED(msm_id);
@@ -53,14 +53,14 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    fp = fopen("/proc/app_info", "rb");
-    while (fgets(model, 100, fp))
-        if (strstr(model, "huawei_fac_product_name") != NULL)
+    fin.open("/proc/app_info");
+    while (getline(fin, buf))
+        if (buf.find("huawei_fac_product_name") != std::string::npos)
             break;
-    fclose(fp);
+    fin.close();
 
     /* C8817D */
-    if (strstr(model, "C8817D") != NULL) {
+    if (buf.find("C8817D") != std::string::npos) {
         property_set("ro.product.model", "C8817D");
         property_set("ro.product.device", "C8817D");
         property_set("ro.build.product", "C8817D");
@@ -69,7 +69,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Huawei/C8817D/hwC8817D:4.4.4/HuaweiC8817D/C92B381:user/release-keys");
     }
     /* C8817E */
-    else if (strstr(model, "C8817E") != NULL) {
+    else if (buf.find("C8817E") != std::string::npos) {
         property_set("ro.product.model", "C8817E");
         property_set("ro.product.device", "C8817E");
         property_set("ro.build.product", "C8817E");
@@ -77,7 +77,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Huawei/C8817E/hwC8817E:4.4.4/HuaweiC8817E/C92B380:user/release-keys");
     }
     /* G621-TL00 */
-    else if (strstr(model, "G621-TL00") != NULL) {
+    else if (buf.find("G621-TL00") != std::string::npos) {
         property_set("ro.product.model", "G621-TL00");
         property_set("ro.product.device", "G621-TL00");
         property_set("ro.build.product", "G621-TL00");
@@ -86,7 +86,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Honor/G621-TL00/hwG621-TL00:4.4.4/HonorG621-TL00/C01B361SP01:user/release-keys");
     }
     /* G620S-UL00 */
-    else if (strstr(model, "G620S-UL") != NULL) {
+    else if (buf.find("G620S-UL") != std::string::npos) {
         property_set("ro.product.model", "G620S-UL00");
         property_set("ro.product.device", "G620S-UL00");
         property_set("ro.build.product", "G620S-UL00");
@@ -95,15 +95,23 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Huawei/G620S-UL00/hwG620S-UL00:4.4.4/HuaweiG620S-UL00/C17B246:user/release-keys");
     }
     /* G620S-L01 */
-    else if (strstr(model, "G620S-L01") != NULL) {
+    else if (buf.find("G620S-L01") != std::string::npos) {
         property_set("ro.product.model", "G620S-L01");
         property_set("ro.product.device", "G620S-L01");
         property_set("ro.build.product", "G620S-L01");
         property_set("ro.build.description", "G620S-L01-user 4.4.4 GRJ90 C00B380 release-keys");
         property_set("ro.build.fingerprint", "Huawei/G620S-L01/hwG620S-L01:4.4.4/HuaweiG620S-L01/C00B380:user/release-keys");
     }
+    /* G620S-L02 */
+    else if (buf.find("G620S-L02") != std::string::npos) {
+        property_set("ro.product.model", "G620S-L02");
+        property_set("ro.product.device", "G620S-L02");
+        property_set("ro.build.product", "G620S-L02");
+        property_set("ro.build.description", "G620S-L02-user 4.4.4 GRJ90 C00B380 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/G620S-L02/hwG620S-L02:4.4.4/HuaweiG620S-L02/C00B380:user/release-keys");
+    }
     /* Che1-CL10 */
-    else if (strstr(model, "Che1-CL10") != NULL) {
+    else if (buf.find("Che1-CL10") != std::string::npos) {
         property_set("ro.product.model", "Che1-CL10");
         property_set("ro.product.device", "Che1-CL10");
         property_set("ro.build.product", "Che1-CL10");
@@ -112,7 +120,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Honor/Che1-CL10/Che1:4.4.4/Che1-CL10/C92B275:user/ota-rel-keys,release-keys");
     }
     /* Che1-CL20 */
-    else if (strstr(model, "Che1-CL20") != NULL) {
+    else if (buf.find("Che1-CL20") != std::string::npos) {
         property_set("ro.product.model", "Che1-CL20");
         property_set("ro.product.device", "Che1-CL20");
         property_set("ro.build.product", "Che1-CL20");
@@ -121,7 +129,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Honor/Che1-CL20/Che1:4.4.4/Che1-CL20/C00B275:user/ota-rel-keys,release-keys");
     }
     /* Che1-L04 */
-    else if (strstr(model, "Che1-L04") != NULL) {
+    else if (buf.find("Che1-L04") != std::string::npos) {
         property_set("ro.product.model", "Che1-L04");
         property_set("ro.product.device", "Che1-L04");
         property_set("ro.build.product", "Che1-L04");
