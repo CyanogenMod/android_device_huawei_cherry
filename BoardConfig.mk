d0dcd32c4cfde192350e53fe97cca0b4ef3a10f6
dianlujitao <dianlujitao@gmail.com>
2016-05-30 13:04:47 +0800

    cherry: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# inherit from msm8916-common
+-include device/huawei/msm8916-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/huawei/cherry
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := c8817d,g620s,C8817D,C8817E,G621-TL00,G620S-UL00,G620S-L01,Che1-CL20,Che1-L04
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
+
+# inherit from the proprietary version
+-include vendor/huawei/cherry/BoardConfigVendor.mk
---
+++ releasetools/device_check.sh
@@ -0,0 +1,34 @@
+#!/sbin/sh
+#
+# Copyright (C) 2016 CyanogenMod Project
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MODEL=`getprop ro.product.model`
+NFC_MODEL="C8817D C8817E G620S-L01"
+
+RESULT=$(echo $NFC_MODEL | grep "$PRODUCT_MODEL")
+
+if [[ "$RESULT" = "" ]]
+then
+    # Remove NFC
+    rm -rf /system/app/QNfc
+    rm -rf /system/priv-app/Tag
+    rm -rf /system/lib/*nfc*
+    rm -rf /system/etc/*nfc*
+    rm -rf /system/etc/permissions/*nfc*
+    rm -rf /system/framework/*nfc*
+    rm -rf /system/vendor/lib/*nfc*
+fi
